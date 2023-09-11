25199209fafd3ed4733d9f22c14dad3120406cd9
renato severo <renatoosevero@gmail.com>
2023-09-11 18:15:30 -0300

    Desafio Controle de Fluxo

---
+++ src/main/java/org/example/Contador.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("ERRO: O segundo parâmetro deve ser maior que o primeiro.");
+        }
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo o número: " + i);
+        }
+
+    }
+}
---
+++ src/main/java/org/example/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+}
